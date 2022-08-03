6f3a0e6489e282e674d36623a3c956981ca85cd4
diego <diegozaratean@gmail.com>
2022-08-03 11:07:29 -0500

    add content types restaurant, categories and components Galery Block text

---
+++ src/api/catebory/routes/catebory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catebory router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::catebory.catebory');
---
+++ src/api/catebory/services/catebory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * catebory service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::catebory.catebory');
